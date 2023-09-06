6d9dfec5f87529acb5879c52dcf30e30f12790c8
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2023-09-06 16:21:32 +0300

    first working version

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function playRound(playerSelection, computerSelection) {
                 return 'PC wins'
             }
             else if (c === 'scissors'){
-                return 'Players wins'
+                return 'Player wins'
             }
             break
         case 'paper':
@@ -20,7 +20,7 @@ function playRound(playerSelection, computerSelection) {
                 return 'PC wins'
             }
             else if (c === 'rock'){
-                return 'Players wins'
+                return 'Player wins'
             }
             break
         case 'scissors':
@@ -28,7 +28,7 @@ function playRound(playerSelection, computerSelection) {
                 return 'PC wins'
             }
             else if (c === 'paper'){
-                return 'Players wins'
+                return 'Player wins'
             }
             break
     }
@@ -41,7 +41,38 @@ function getComputerChoice() {
 }
 
    
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+let playerSelection = "";
+
+let p_score_counter = 0
+let c_score_counter = 0 
+
+let paper = document.querySelector('#paper')
+let rock = document.querySelector('#rock')
+let scissors = document.querySelector('#scissors')
+let start = document.querySelector('#start')
+let p_score = document.querySelector('#player-score')
+let c_score = document.querySelector('#pc-score')
+
+paper.onclick = () => playerSelection = 'paper'
+rock.onclick = () => playerSelection = 'rock'
+scissors.onclick = () => playerSelection = 'scissors'
+
+start.onclick = () => {
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection)
+
+
+    let score = playRound(playerSelection , computerSelection)
+
+    if (score.toLowerCase() === 'player wins'){
+        p_score_counter++
+    }
+    else if (score.toLowerCase() === 'pc wins'){
+        c_score_counter++
+    }
+
+    p_score.textContent = `Player Score : ${p_score_counter}`
+    c_score.textContent = `Computer score : ${c_score_counter}`
+
+
+}
